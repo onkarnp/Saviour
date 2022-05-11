1b27d1860e8f37faa5b328d1ca0f3f0defdc025e
sumitk-15 <sumit.koundanya@walchandsangli.ac.in>
2022-05-11 18:44:15 +0530

    Sign Up Activity

---
+++ app/src/main/java/com/example/saviour/activity_signup.java
@@ -0,0 +1,45 @@
+package com.example.saviour;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.TextView;
+import android.widget.Toast;
+
+public class activity_signup extends AppCompatActivity {
+
+    EditText name,phoneno,email,adhaar,address,pincode;
+    ImageView logo;
+    Button signup;
+    TextView alreadyRes;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_signup);
+
+        name = findViewById(R.id.signup_name);
+        phoneno = findViewById(R.id.signup_phoneno);
+        email = findViewById(R.id.signup_email);
+        adhaar = findViewById(R.id.signup_adhaar);
+        address = findViewById(R.id.signup_address);
+        pincode = findViewById(R.id.signup_pincode);
+
+        signup = findViewById(R.id.registerBtn);
+        alreadyRes = findViewById(R.id.alreadyRes);
+        logo = findViewById(R.id.imageButton);
+
+    }
+
+    public void Homepage_activity(View view) {
+
+        Toast toast = Toast.makeText(activity_signup.this,"Registered Successfully",Toast.LENGTH_SHORT);
+        toast.show();
+        Intent intent = new Intent(this,activity_signup.class);
+    }
+}
