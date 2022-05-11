df21caae08a9ab120b6ff560f51498b1a768e1c1
Sumitkhandate <sumitkhandate18@gmail.com>
2022-05-12 02:30:41 +0530

    homepage modified

---
+++ app/src/main/java/com/example/saviour/Common_queries_card.java
@@ -0,0 +1,14 @@
+package com.example.saviour;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Common_queries_card extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_common_queries_card);
+    }
+}
