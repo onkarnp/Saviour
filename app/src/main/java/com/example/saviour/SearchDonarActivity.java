7083ae2527dc8e61676ca186006958c01f4ba60c
onkarnp <opanchare123@gmail.com>
2022-05-12 06:55:16 +0530

    Adapters added

--- app/src/main/java/com/example/saviour/SearchDonarActivity.java
+++ app/src/main/java/com/example/saviour/SearchDonarActivity.java
@@ -1,10 +1,27 @@
 package com.example.saviour;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.cardview.widget.CardView;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
+import android.widget.AutoCompleteTextView;
+import android.widget.EditText;
+
+import com.example.saviour.models.User;
+import com.google.firebase.database.FirebaseDatabase;
+
+import java.util.ArrayList;
 
 public class SearchDonarActivity extends AppCompatActivity {
+
+    FirebaseDatabase database;
+    private EditText pin_code;
+    AutoCompleteTextView bloodgrp;
+    CardView search;
+    RecyclerView recyclerView;
+//    MyAdapter1 myAdapter;
+    ArrayList<User> list;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
